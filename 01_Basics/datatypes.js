bbb265ae4819cf3a5e6ff3920e9ac2deeadf8503
Mayur Bankar <127082849+MayurBankar5757@users.noreply.github.com>
2023-08-10 16:46:30 +0000

    container config done

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,15 @@
+//data type
+//numbers 2 to power 53
+//bigint
+let c = "hemant"    // String
+ let v = true   // boolean => true or false
+  let d = null;// null => standolone value which having object type
+ let k ={
+    name: "Mayur",
+    age : 25
+ }//object 
+//undefined its type also undefined
+// symbol => unique
+ console.log(typeof d)//object
+ console.log(typeof k)// object
+ 
