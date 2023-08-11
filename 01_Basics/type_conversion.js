eaf5f8978a72eb4ce39409643cd40fad19aa1830
Mayur Bankar <127082849+MayurBankar5757@users.noreply.github.com>
2023-08-11 14:26:19 +0000

    container config done

---
+++ 01_Basics/type_conversion.js
@@ -0,0 +1,9 @@
+let name = "100"
+console.log(typeof name) //string
+let c = Number(name) // conversion to string 
+//console.log(typeof c) // number
+console.log(c)  // vlaue => NaN
+let g= 1   
+//console.log(typeof g) // number 
+//let p = Boolean (g)
+//console.log(typeof p) // boolean
