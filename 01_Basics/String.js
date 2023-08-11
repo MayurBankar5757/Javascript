3cd901ab3d2e13357327df1940c70cb55362e4d1
Mayur Bankar <127082849+MayurBankar5757@users.noreply.github.com>
2023-08-11 15:30:03 +0000

    container config done

---
+++ 01_Basics/String.js
@@ -0,0 +1,31 @@
+let Name = "hitesh"
+//console.log(typeof Name) // string
+
+//*******    operations ******/
+
+let value =3
+let Negvalue = -value
+//console.log(Negvalue)
+
+let str1 = "Hello"
+let str2 = " Hitesh"
+// console.log(str1+str2)
+
+console.log ("1" + 2)
+console.log (1 + "2")
+console.log ("1" + 2 + 2)
+console.log (1 + 2 + "2")
+console.log (+true)
+// console.log (true +) error
+console.log (+false)
+// console.log (false+) unexpected token error
+console.log (+"")
+
+ let gameCounter = 100
+// gameCounter++
+// console.log(gameCounter)
+console.log(gameCounter++)
+console.log(gameCounter)
+console.log(++ gameCounter)
+console.log(gameCounter)
+
